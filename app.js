4c8017569f8462af79fe248e0f21420c77dfc2bc
iNFiENiTE-World <infienite@gmail.com>
2023-02-12 07:51:15 +0800

    Loaded, parsed and saved JSON file

--- app.js
+++ app.js
@@ -1,3 +1,12 @@
-const chalk = require('chalk')
-const yargs = require('yargs')
-const { getNotes } = require('./notes')
+const fs = require('fs')
+
+const dataBuffer = fs.readFileSync('data.json').toString()
+const dataJSON = dataBuffer.toString()
+const data = JSON.parse(dataJSON)
+
+data.name = "iNFiENiTE"
+data.planet = "MyEarth"
+data.age = 18
+
+const updatedData = JSON.stringify(data)
+fs.writeFileSync('data.json', updatedData)
