aa9deb0f8de5d9a4aa39f332855987b2aea12c12
iNFiENiTE-World <infienite@gmail.com>
2023-02-11 18:00:13 +0800

    Initialized builder properties for add command, not implemented yet

--- app.js
+++ app.js
@@ -4,36 +4,52 @@ const { getNotes } = require('./notes')
 
 // Add new note
 yargs.command({
-    command: "add",
-    describe: "Add a new note",
-    handler: () => {
-        console.log(chalk.blueBright("Adding note..."))
+    command: 'add',
+    describe: 'Add a new note',
+    builder: {
+        title: {
+            describe: 'Note title',
+            demandOption: true,
+            type: 'string'
+        },
+        description: {
+            describe: 'Note content',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler: function (argv) {
+        console.log(chalk.blueBright('Adding'), chalk.blueBright.italic(`${argv.title}...`))
+        console.log(chalk.dim(`Title: ${argv.title}\nDescription: ${argv.description}`));
     }
 })
 
 // List all notes
 yargs.command({
-    command: "list",
-    describe: "List out all notes",
+    command: 'list',
+    describe: 'List out all notes',
     handler: () => {
-        console.log(chalk.greenBright("Loading..."));
+        console.log(chalk.greenBright('Loading...'));
     }
 })
 
 // Read a note
 yargs.command({
-    command: "read",
-    describe: "Display a note",
+    command: 'read',
+    describe: 'Display a note',
     handler: () => {
-        console.log(chalk.greenBright("Loading..."));
+        console.log(chalk.greenBright('Loading...\n'))
+        console.log(chalk.bold('Your note\n'), getNotes())
     }
 })
 
 // Remove a note
 yargs.command({
-    command: "remove",
-    describe: "Remove a note",
-    hadnler: () => {
-        console.log(chalk.red("Removing note..."));
+    command: 'remove',
+    describe: 'Remove a note',
+    handler: () => {
+        console.log(chalk.redBright('Removing note...'));
     }
 })
+
+yargs.parse()
