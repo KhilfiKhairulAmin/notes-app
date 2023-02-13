80518b421660d205f59504d6506e58374c7bfb17
iNFiENiTE-World <infienite@gmail.com>
2023-02-13 19:31:26 +0800

    Documented core functionalities with proper JSDocs format

--- notes.js
+++ notes.js
@@ -6,11 +6,17 @@ const getNotes = function () {
     console.log('Loading your notes...');
 }
 
+/**
+ * Add a new note and save it
+ * @param {String} title Title of note
+ * @param {String} content Content of note
+ * @returns 
+ */
 const addNote = function (title, content) {
     // Load notes
     const notes = loadNotes()
 
-    // Handle duplicate title and find insertion index to keep data sorted (ascending order)
+    // Handle duplicate title and find insertion index to keep data sorted (in ascending order)
     // This is a binary search implementation, where two pointers are used. Go and search if you don't know what to imagine with this code
     let l = 0, r = notes.length - 1
     while (l <= r) {
@@ -41,14 +47,21 @@ const addNote = function (title, content) {
     console.log(chalk.greenBright('Note added successfully!'))
 }
 
+/**
+ * Remove a note
+ * @param {String} title Title of note
+ * @returns 
+ */
 const removeNote = function (title) {
     const notes = loadNotes()
 
+    // Search for title using binary search
     let l = 0, r = notes.length - 1
     while (l <= r) {
         const mid = Math.floor((l + r) / 2)
 
         if (notes[mid].title === title) {
+            // Title is found, remove it
             console.log(chalk.red.bold(`Removing ${title}...`))
             notes.splice(mid, 1)
             saveNotes(notes)
@@ -64,14 +77,23 @@ const removeNote = function (title) {
         }
     }
 
+    // Title is not found
     console.log(chalk.yellow(`Note title does not exist. No note removed.`))
 }
 
+/**
+ * Save notes in `notes.json` _(data source file)_
+ * @param {Array} notes Notes array
+ */
 const saveNotes = function (notes) {
     const dataJSON = JSON.stringify(notes)
     writeFileSync('notes.json', dataJSON)
 }
 
+/**
+ * Load notes from `notes.json` _(data source file)_
+ * @returns {Array} Notes array
+ */
 const loadNotes = function () {
     try {
         const dataBuffer = readFileSync('notes.json')
